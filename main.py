cb06e60dd6d06e8489898650c706e9fe0a1d519b
Morgan <morganpsyinfo@gmail.com>
2025-01-15 15:27:18 +0100

    test

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 ma_variable = "Hola"
+
+
+def blabla(kiki):
+    if not kiki[2]:  # pause_status
+        new_now = kiki[0]
+        state = (new_now, state[1], state[2], state[3])  
+    return state 
