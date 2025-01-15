24b861d50d51e1672317435fc9aeda0827d95ec1
Morgan <morganpsyinfo@gmail.com>
2025-01-15 15:25:45 +0100

    test

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 ma_variable = "Hola"
+
+
+def seconds_thread(state):
+    if not state[2]:  # pause_status
+        new_now = state[0] 
+        state = (new_now, state[1], state[2], state[3])  
+    return state 
